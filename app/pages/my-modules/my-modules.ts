644b3e78b84488bd3d286af3706c7f9bb36579d8
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2016-04-30 19:10:42 +0200

    ref (ILecture): Ilecture removed

--- app/pages/my-modules/my-modules.ts
+++ app/pages/my-modules/my-modules.ts
@@ -2,24 +2,14 @@ import {Page, NavController} from 'ionic-angular';
 import {MyLecturesService} from '../../services/my-lectures.service.ts';
 
 import {LectureDetailPage} from '../lecture-detail/lecture-detail';
-
-interface ILecture {
-  idlecture: number,
-  starttime: string,
-  endtime: string,
-  building: number,
-  room: string,
-  classname: string,
-  responsible: string,
-  moduleId: number
-}
+import {Lecture} from "../../classes/lecture.class";
 
 @Page({
   templateUrl: 'build/pages/my-modules/my-modules.html',
   providers: [MyLecturesService]
 })
 export class MyModulesPage {
-  myLectures: Array<{ id: number, c: ILecture }>;
+  myLectures: Array<{ id: number, c: Lecture }>;
 
   constructor(private nav: NavController, _myLecturesService: MyLecturesService) {
     this.myLectures = _myLecturesService.getLectures();
---
+++ app/services/menu.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from 'angular2/core';
+import {Http, Headers, RequestOptions} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class MenuService {
+    http: Http;
+
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    getLecturesByModule(id: number): Observable<any> {
+        return this.http.get('https://www.cs.technik.fhnw.ch/App4Technik/mensa/')
+            .map(this.extractDataMenues)
+            .catch(this.handleError);
+    }
+
+
+    private extractDataMenues(res) {
+        if (res.status < 200 || res.status >= 300) {
+            throw new Error('Bad response status: ' + res.status);
+        }
+        let body = res.json();
+
+        console.log(body);
+        return body.menues || { };
+    }
+
+
+    private handleError (error: any) {
+        // In a real world app, we might send the error to remote logging infrastructure
+        let errMsg = error.message || 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
+}
