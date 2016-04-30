606dd6423e5624b884433efc1444b6ce62d99f9b
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2016-04-30 20:57:01 +0200

    style (module): Design changes

--- app/pages/module-detail/module-detail.ts
+++ app/pages/module-detail/module-detail.ts
@@ -5,6 +5,7 @@ import {LectureService} from '../../services/lecture.service';
 
 import {LectureDetailPage} from '../lecture-detail/lecture-detail';
 import {AddLectureModalPage} from "../add-lecture-modal/add-lecture-modal";
+import {Lecture} from "../../classes/lecture.class";
 
 interface ILecture {
     idlecture: number,
@@ -18,61 +19,55 @@ interface ILecture {
 }
 
 @Page({
-  templateUrl: 'build/pages/module-detail/module-detail.html',
-  providers: [ModuleService, MyLecturesService, LectureService]
+    templateUrl: 'build/pages/module-detail/module-detail.html',
+    providers: [ModuleService, MyLecturesService, LectureService]
 })
 export class ModuleDetailPage {
-  selectedItem: number;
-  id: number;
-  short: string;
-  description: string;
-  lectures: Array<ILecture>;
+    selectedItem:number;
+    id:number;
+    short:string;
+    description:string;
+    lectures:Array<ILecture>;
 
-  _myLecturesService: MyLecturesService;
+    _myLecturesService:MyLecturesService;
 
-  addLectureModalPage: typeof AddLectureModalPage;
-    lectureParams: {idmodule: number};
+    addLectureModalPage:typeof AddLectureModalPage;
+    lectureParams:{idmodule: number};
 
-  constructor(private nav: NavController, navParams: NavParams, _moduleService: ModuleService, _myLecturesService: MyLecturesService, _lectureService: LectureService) {
-    this.nav = nav;
-    this._myLecturesService = _myLecturesService;
+    constructor(private nav:NavController, navParams:NavParams, _moduleService:ModuleService, _myLecturesService:MyLecturesService, _lectureService:LectureService) {
+        this.nav = nav;
+        this._myLecturesService = _myLecturesService;
 
-    this.addLectureModalPage = AddLectureModalPage;
+        this.addLectureModalPage = AddLectureModalPage;
 
-    this.selectedItem = navParams.get('moduleId');
-      this.lectureParams = {idmodule: this.selectedItem};
+        this.selectedItem = navParams.get('moduleId');
+        this.lectureParams = {idmodule: this.selectedItem};
 
-    _moduleService.getModuleById(this.selectedItem)
-        .subscribe(
-            module => {
-              this.id = module.idmodule;
-              this.short = module.short;
-              this.description = module.description;
-            },
-            error =>  console.log(error)
-        );
+        _moduleService.getModuleById(this.selectedItem)
+            .subscribe(
+                module => {
+                    this.id = module.idmodule;
+                    this.short = module.short;
+                    this.description = module.description;
+                },
+                error => console.log(error)
+            );
 
-    _lectureService.getLecturesByModule(this.selectedItem)
-        .subscribe(
-            lectures => {
-              this.lectures = lectures;
-            },
-            error =>  console.log(error)
-        );
-  }
+        _lectureService.getLecturesByModule(this.selectedItem)
+            .subscribe(
+                lectures => {
+                    this.lectures = lectures;
+                },
+                error => console.log(error)
+            );
+    }
 
-  hasLecture(id: number) {
-    return this._myLecturesService.hasLecture(id);
-  }
+    hasLecture(id:number) {
+        return this._myLecturesService.hasLecture(id);
+    }
 
-  toggleLecture(c: ILecture) {
-    this._myLecturesService.toggleLecture(c.idlecture, c);
-  }
-
-    itemTapped(event, id) {
-        this.nav.push(LectureDetailPage, {
-            lectureId: id
-        })
+    toggleLecture(c:Lecture) {
+        this._myLecturesService.toggleLecture(c.idlecture, c);
     }
 
 }
